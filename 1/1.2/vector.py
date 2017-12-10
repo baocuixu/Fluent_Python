7c8668d00a4a21c59b6543118cc453f75c3adb9e
xubaocui <xubaocui0207@sina.com>
2017-12-10 15:53:59 +0800

    update

--- 1/1.2/vector.py
+++ 1/1.2/vector.py
@@ -12,6 +12,10 @@ class Vector():
 		self.y = y
 	
 	def __repr__(self):
+		'''
+		将一个对象用字符串的形式表现出来
+		如果没有实现该函数，打印对象时，会是<Vector object at 0x10e100070>这种格式
+		'''
 		return "Vector(%r, %r)" % (self.x, self.y)
 
 	def __abs__(self):
