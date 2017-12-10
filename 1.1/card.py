4185d19ee1ac5aeec93098a0284df8b0a8902e55
xubaocui <xubaocui0207@sina.com>
2017-12-10 13:31:34 +0800

    1.1

---
+++ 1.1/card.py
@@ -0,0 +1,19 @@
+#! -*- coding: utf-8 -*-
+
+import collections
+
+Card = collections.nametuple('Card',['rank', 'suit'])
+
+class FrenchDeck:
+	ranks = [str(n) for n in range(2, 11) + list('JQKA')]
+	suits = 'spades diamonds clubs hearts'.split()
+
+	def __init__(self):
+		self._cards = [Card(rank suit) for suit in self.suits
+		                               for rank self.ranks]
+	
+	def __len__(self):
+		return len(self._cards)
+
+	def __getitem__(self, postition)：
+		return self._cards[postion]
